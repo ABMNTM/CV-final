08299c55204ce0802b63a376801772b6d0e9fce3
abolfazl <abolfazl@allah.sysroad>
2026-02-23 09:05:33 -0500

    Phase 2 started.

---
+++ phase2/main.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+from scipy.stats import cumfreq
+
+
+class ImageEnhancer:
+    def __get_lab(self, img):
+        lab_img = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+        return cv.split(lab_img)
+    
+    def __get_bgr(self, l, a, b):
+        enhanced_lab = cv.merge((l, a, b))
+        return cv.cvtColor(enhanced_lab, cv.COLOR_LAB2BGR)
+
+    def histogram_equalization(self, img):
+        """ img is BGR """
+        l, a, b = self.__get_lab(img)
+        l_enhanced = cv.equalizeHist(l)
+        return self.__get_bgr(l_enhanced, a, b)
+
+    def CLAHE(self, img):
+        """ img is BGR """
+        l, a, b = self.__get_lab(img)
+        clahe = cv.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+        l_enhanced = clahe.apply(l)
+        return self.__get_bgr(l_enhanced, a, b)
+    
+    
+    
